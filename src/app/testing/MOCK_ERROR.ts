bed2ba00bfaf6b3f8bbf25ebdaf5c9f8b2c4cf40
git-kevin-balauro <kevinbalauro@hotmail.com>
2025-10-29 15:26:44 +0800

    create mock news error file

---
+++ src/app/testing/MOCK_ERROR.ts
@@ -0,0 +1,11 @@
+export const MOCK_ERROR = {
+    "error": {
+        "code": "validation_error",
+        "message": "Validation error",
+        "context": {
+            "date": [
+                "NO_SUCH_CHOICE_ERROR"
+            ]
+        }
+    }
+}
